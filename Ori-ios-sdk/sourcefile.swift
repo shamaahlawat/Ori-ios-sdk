95c0b1e8483ec7d7f8e713881fa1d19b271c81ed
Shama Ahlawat <shama@oriserve.com>
2018-12-14 19:51:23 +0530

    webview is added and info.plist also modified and url is passed to webview

--- Ori-ios-sdk/sourcefile.swift
+++ Ori-ios-sdk/sourcefile.swift
@@ -7,11 +7,18 @@
 //
 
 import Foundation
+import WebKit
 
 public class Service {
-    @IBOutlet weak var button: UIButton!
-    private init() {}
+   @IBOutlet weak var webview: WKWebView!
+    private init() {
+        let htmlpath = Bundle.main.path(forResource: "index", ofType: "html")
+        let url = URL(fileURLWithPath : htmlpath!)
+        let request = URLRequest(url: url)
+        webview.load(request)
+    }
     public static func doSomething() -> String {
         return "do something for us"
     }
 }
+
